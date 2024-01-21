2b3803c8c7de48c64fbada14f7fb4cc01a5d341b
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-21 22:59:57 +0900

    feat: Replace with runSlideApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
 import 'dart:io';
 
+import 'package:dvm/app_slides.dart';
+import 'package:dvm/app_theme.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:slidex/slidex.dart';
 import 'package:window_size/window_size.dart';
 
-void main() {
+void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
   if (!kIsWeb || Platform.isMacOS) {
     setWindowMinSize(const Size(640, 360));
   }
   usePathUrlStrategy();
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runSlideApp(
+    args: args,
+    theme: appTheme,
+    slides: appSlides,
+  );
 }
