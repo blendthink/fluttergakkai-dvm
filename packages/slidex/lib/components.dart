7f88c8b2fdc0aad1e9f00e56bfe57c1bd8423a51
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-21 22:15:31 +0900

    feat: Add SlideWidget export

--- packages/slidex/lib/components.dart
+++ packages/slidex/lib/components.dart
@@ -2,3 +2,4 @@ export 'src/components/highlight/highlight_theme.dart';
 export 'src/components/highlight/highlight_view.dart';
 export 'src/components/highlight/language.dart';
 export 'src/components/scaler_gap.dart';
+export 'src/components/slide_widget.dart';
