fd3d11fc53ab7d7143f56ef4355b4f97ed0271d6
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-21 22:10:45 +0900

    feat: Create runSlideApp method

--- packages/slidex/lib/slidex.dart
+++ packages/slidex/lib/slidex.dart
@@ -1,5 +1,1 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/run_slide_app.dart';
---
+++ packages/slidex/lib/src/run_slide_app.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:slidex/src/components/slide_widget.dart';
+import 'package:slidex/src/internal/app/presentation_app.dart';
+import 'package:slidex/src/internal/app/slide_app.dart';
+
+void runSlideApp({
+  required List<String> args,
+  required String title,
+  required ThemeData theme,
+  required List<SlideWidget> slides,
+}) {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (args.firstOrNull == 'multi_window' && !kIsWeb) {
+    final argText = args[2];
+    final argMap = jsonDecode(argText) as Map<String, dynamic>?;
+    final initSlideIndex = argMap?['initSlideIndex'] as int? ?? 0;
+    runApp(
+      PresentationApp(
+        initSlideIndex: initSlideIndex,
+        slides: slides,
+      ),
+    );
+    return;
+  }
+
+  final app = SlideApp(
+    title: title,
+    theme: theme,
+    slides: slides,
+  );
+  runApp(app);
+}
