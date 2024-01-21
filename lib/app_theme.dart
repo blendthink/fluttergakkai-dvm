480240d73848e9b59ae740662e8f227a8253f4f9
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-21 22:57:33 +0900

    feat: Create app theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+final appTheme = _baseTheme.copyWith(
+  dividerTheme: _baseTheme.dividerTheme.copyWith(
+    color: _appColorScheme.outline,
+  ),
+);
+
+final _baseTheme = ThemeData.from(
+  colorScheme: _appColorScheme,
+  useMaterial3: true,
+);
+
+const _appColorScheme = ColorScheme(
+  brightness: Brightness.dark,
+  primary: Color(0xFFD0BCFF),
+  onPrimary: Color(0xFF381E72),
+  primaryContainer: Color(0xFF4F378B),
+  onPrimaryContainer: Color(0xFFEADDFF),
+  secondary: Color(0xFFCCC2DC),
+  onSecondary: Color(0xFF332D41),
+  secondaryContainer: Color(0xFF4A4458),
+  onSecondaryContainer: Color(0xFFE8DEF8),
+  tertiary: Color(0xFFEFB8C8),
+  onTertiary: Color(0xFF492532),
+  tertiaryContainer: Color(0xFF633B48),
+  onTertiaryContainer: Color(0xFFFFD8E4),
+  error: Color(0xFFF2B8B5),
+  onError: Color(0xFF601410),
+  errorContainer: Color(0xFF8C1D18),
+  onErrorContainer: Color(0xFFF9DEDC),
+  outline: Color(0xFF938F99),
+  background: Color(0xFF141218),
+  onBackground: Color(0xFFE6E0E9),
+  surface: Color(0xFF141218),
+  onSurface: Color(0xFFFFFFFF),
+  surfaceVariant: Color(0xFF49454F),
+  onSurfaceVariant: Color(0xFFCAC4D0),
+  inverseSurface: Color(0xFFE6E0E9),
+  onInverseSurface: Color(0xFF322F35),
+  inversePrimary: Color(0xFF6750A4),
+  shadow: Color(0xFF000000),
+  surfaceTint: Color(0xFFD0BCFF),
+  outlineVariant: Color(0xFF49454F),
+  scrim: Color(0xFF000000),
+);
