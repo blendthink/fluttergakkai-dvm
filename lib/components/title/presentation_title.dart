241ace3ccfe23175973352aef2ef07e867bedc4e
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-22 23:44:14 +0900

    improve: Update presentation title text and title style

--- lib/components/title/presentation_title.dart
+++ lib/components/title/presentation_title.dart
@@ -7,8 +7,10 @@ final class PresentationTitle extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Text(
-      'Dart Version Manager (DVM) を\n開発してみたお話',
-      style: theme.textTheme.headlineLarge,
+      'Dart Version Manager (DVM) を\n開発したお話',
+      style: theme.textTheme.headlineLarge?.copyWith(
+        height: 1.4,
+      ),
     );
   }
 }
