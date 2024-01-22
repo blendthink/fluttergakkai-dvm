ca9944a0558cf1a618a27c498b25951ec8d05208
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-22 19:50:29 +0900

    feat: Add IntroSlide to appSlides

--- lib/app_slides.dart
+++ lib/app_slides.dart
@@ -1,6 +1,8 @@
+import 'package:dvm/slides/intro_slide.dart';
 import 'package:dvm/slides/title_slide.dart';
 import 'package:slidex/components.dart';
 
 const appSlides = <SlideWidget>[
   TitleSlide(),
+  IntroSlide(),
 ];
