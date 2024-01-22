b4c4933eecf44152268ccafa3af63816cb4707a5
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-22 18:02:39 +0900

    fix: Move flutter_web_plugins, window_size packages to slidex

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
-import 'dart:io';
-
 import 'package:dvm/app_slides.dart';
 import 'package:dvm/app_theme.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:slidex/slidex.dart';
-import 'package:window_size/window_size.dart';
 
 void main(List<String> args) {
-  WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    usePathUrlStrategy();
-  }
-
-  if (!kIsWeb && Platform.isMacOS) {
-    setWindowMinSize(const Size(640, 360));
-  }
-
   runSlideApp(
     args: args,
     theme: appTheme,
--- packages/slidex/lib/src/run_slide_app.dart
+++ packages/slidex/lib/src/run_slide_app.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:slidex/src/components/slide_widget.dart';
 import 'package:slidex/src/internal/app/presentation_app.dart';
 import 'package:slidex/src/internal/app/slide_app.dart';
+import 'package:window_size/window_size.dart';
 
 void runSlideApp({
   required List<String> args,
@@ -12,6 +15,7 @@ void runSlideApp({
   required List<SlideWidget> slides,
 }) {
   WidgetsFlutterBinding.ensureInitialized();
+
   if (args.firstOrNull == 'multi_window' && !kIsWeb) {
     final argText = args[2];
     final argMap = jsonDecode(argText) as Map<String, dynamic>?;
@@ -23,6 +27,14 @@ void runSlideApp({
       ),
     );
     return;
+  }
+
+  if (kIsWeb) {
+    usePathUrlStrategy();
+  }
+
+  if (!kIsWeb && Platform.isMacOS) {
+    setWindowMinSize(const Size(640, 360));
   }
 
   final app = SlideApp(
