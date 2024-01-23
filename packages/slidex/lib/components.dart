87eb7eeef1351994b4c34d7b683eac4896dee9dc
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-24 00:20:55 +0900

    feat: Exportd ScalerPadding component

--- packages/slidex/lib/components.dart
+++ packages/slidex/lib/components.dart
@@ -3,4 +3,5 @@ export 'src/components/highlight/highlight_view.dart';
 export 'src/components/highlight/language.dart';
 export 'src/components/scaler_circle_avatar.dart';
 export 'src/components/scaler_gap.dart';
+export 'src/components/scaler_padding.dart';
 export 'src/components/slide_widget.dart';
