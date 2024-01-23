67f3c110fef0493e28e0574624f7fd8fa2989407
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-23 18:36:39 +0900

    feat: Add slide navigation by shortcuts

--- packages/slidex/lib/src/internal/app/presentation_app.dart
+++ packages/slidex/lib/src/internal/app/presentation_app.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:slidex/src/components/slide_widget.dart';
+import 'package:slidex/src/internal/slide_intents.dart';
 
 final class PresentationApp extends StatefulWidget {
   const PresentationApp({
@@ -43,42 +45,30 @@ class _PresentationAppState extends State<PresentationApp> {
     });
   }
 
+  Future<void> _previous() => DesktopMultiWindow.invokeMethod(0, 'previous');
+
+  Future<void> _next() => DesktopMultiWindow.invokeMethod(0, 'next');
+
   @override
   Widget build(BuildContext context) {
     final speakerNote = widget._slides[_slideIndex].speakerNote;
     return MaterialApp(
+      shortcuts: _shortcuts,
+      actions: <Type, Action<Intent>>{
+        BackIntent: CallbackAction<BackIntent>(
+          onInvoke: (_) async => _previous(),
+        ),
+        NextIntent: CallbackAction<NextIntent>(
+          onInvoke: (_) async => _next(),
+        ),
+      },
       home: Scaffold(
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        unawaited(
-                          DesktopMultiWindow.invokeMethod(0, 'previous'),
-                        );
-                      },
-                      icon: const Icon(Icons.arrow_back),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        unawaited(
-                          DesktopMultiWindow.invokeMethod(0, 'next'),
-                        );
-                      },
-                      icon: const Icon(Icons.arrow_forward),
-                    ),
-                  ],
-                ),
-                Text(
-                  speakerNote,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-              ],
+            padding: const EdgeInsets.all(24),
+            child: Text(
+              speakerNote,
+              style: Theme.of(context).textTheme.titleMedium,
             ),
           ),
         ),
@@ -86,3 +76,8 @@ class _PresentationAppState extends State<PresentationApp> {
     );
   }
 }
+
+const _shortcuts = <ShortcutActivator, Intent>{
+  SingleActivator(LogicalKeyboardKey.arrowLeft): BackIntent(),
+  SingleActivator(LogicalKeyboardKey.arrowRight): NextIntent(),
+};
