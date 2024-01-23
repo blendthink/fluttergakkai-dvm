c160a050ad7a41c19a299e880c67303dbc4e604c
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-23 09:38:38 +0900

    improve: Add support for secure restorable state

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -6,4 +6,8 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
+
+  override func applicationSupportsSecureRestorableState(_ application: NSApplication) -> Bool {
+    return true
+  }
 }
