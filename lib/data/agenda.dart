7e13cec10dd00c389323a573d23f9f43b5aa3e42
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-23 23:22:40 +0900

    feat: Create Agenda class with predefined agenda items

---
+++ lib/data/agenda.dart
@@ -0,0 +1,17 @@
+final class Agenda {
+  const Agenda._();
+
+  static const agenda01 = 'DVM の概要';
+  static const agenda02 = '開発した背景';
+  static const agenda03 = '機能の実装方法';
+  static const agenda04 = '苦労した点';
+  static const agenda05 = '今後の展望';
+
+  static const values = [
+    agenda01,
+    agenda02,
+    agenda03,
+    agenda04,
+    agenda05,
+  ];
+}
