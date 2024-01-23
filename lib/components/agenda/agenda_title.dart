9a0b2068dc253a37bb3e3aa239f5a78844305cdf
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-24 00:48:14 +0900

    feat: Implement highlighting feature in AgendaTitle widget

--- lib/components/agenda/agenda_title.dart
+++ lib/components/agenda/agenda_title.dart
@@ -1,11 +1,18 @@
 import 'package:dvm/data/agenda.dart';
 import 'package:flutter/material.dart';
 import 'package:slidex/components.dart';
+import 'package:slidex/slidex.dart';
 
 final class AgendaTitle extends StatelessWidget {
-  const AgendaTitle(this._agenda, {super.key});
+  const AgendaTitle(
+    this._agenda, {
+    bool isHighlighte = false,
+    super.key,
+  }) : _isHighlighte = isHighlighte;
 
   final Agenda _agenda;
+
+  final bool _isHighlighte;
 
   @override
   Widget build(BuildContext context) {
@@ -13,9 +20,23 @@ final class AgendaTitle extends StatelessWidget {
     return Row(
       children: [
         const ScalerGap(24),
-        Text(
-          '${_agenda.index + 1}. ${_agenda.title}',
-          style: theme.textTheme.titleMedium,
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: _isHighlighte
+                  ? theme.colorScheme.tertiary
+                  : Colors.transparent,
+              width: 2 * context.frameScale,
+              strokeAlign: BorderSide.strokeAlignOutside,
+            ),
+          ),
+          child: ScalerPadding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              '${_agenda.index + 1}. ${_agenda.title}',
+              style: theme.textTheme.titleMedium,
+            ),
+          ),
         ),
       ],
     );
