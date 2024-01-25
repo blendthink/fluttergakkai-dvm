1368f408ed29b11995df900952da9101e797f7e9
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-25 09:09:14 +0900

    feat: Add Agenda01ReleaseSlide to appSlides

--- lib/app_slides.dart
+++ lib/app_slides.dart
@@ -1,6 +1,7 @@
 import 'package:dvm/slides/agenda01/agenda01_figure_slide.dart';
 import 'package:dvm/slides/agenda01/agenda01_fvm_slide.dart';
 import 'package:dvm/slides/agenda01/agenda01_logo_slide.dart';
+import 'package:dvm/slides/agenda01/agenda01_release_slide.dart';
 import 'package:dvm/slides/agenda_slide.dart';
 import 'package:dvm/slides/anime_slide.dart';
 import 'package:dvm/slides/highlighted_agenda01_slide.dart';
@@ -22,6 +23,7 @@ const appSlides = <SlideWidget>[
   Agenda01LogoSlide(),
   Agenda01FvmSlide(),
   Agenda01FigureSlide(),
+  Agenda01ReleaseSlide(),
   HighlightedAgenda02Slide(),
   HighlightedAgenda03Slide(),
   HighlightedAgenda04Slide(),
