eb36503f2c11f2540df286d4510d23ff47c404a8
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-01-25 09:09:06 +0900

    feat: Create Agenda01ReleaseSlide

---
+++ lib/slides/agenda01/agenda01_release_slide.dart
@@ -0,0 +1,60 @@
+import 'package:dvm/components/reference.dart';
+import 'package:dvm/data/agenda.dart';
+import 'package:dvm/gen/assets.gen.dart';
+import 'package:dvm/templates/title_header_slide.dart';
+import 'package:flutter/material.dart';
+import 'package:slidex/components.dart';
+import 'package:slidex/slidex.dart';
+
+final class Agenda01ReleaseSlide extends SlideWidget {
+  const Agenda01ReleaseSlide({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final scale = context.frameScale;
+    final imageWidth = 240 * scale;
+    final body = Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Assets.pubDev.image(
+                width: imageWidth,
+                fit: BoxFit.fitWidth,
+              ),
+              const ScalerGap(8),
+              Reference(
+                uri: Uri.parse('https://pub.dev'),
+              ),
+            ],
+          ),
+          const ScalerGap(32),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Assets.homebrew.image(
+                width: imageWidth,
+                fit: BoxFit.fitWidth,
+              ),
+              const ScalerGap(8),
+              Reference(
+                uri: Uri.parse('https://brew.sh'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+
+    return TitleHeaderSlide(
+      title: Agenda.agenda01.title,
+      body: body,
+    );
+  }
+
+  @override
+  String get speakerNote => '''
+早速、DVM を使ってみたいという方は、 pub.dev と Homebrew tap で公開していますので、ぜひご利用ください。''';
+}
